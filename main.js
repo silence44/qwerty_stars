d25453828e4f4139fc65c474d30a4f162f59f99e
Taras Antonenko <taras.antonenko@wirexapp.com>
2018-03-01 20:49:31 +0200

    parse file

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-/**
- * Created by denys on 01.03.18.
- */
 
 function main() {
-    console.log("Rabotayu blyat'");
+    var fileData;
+
+    function getFileDataByUrl () {
+        return new Promise(function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "inputData/a_example.in");
+            xhr.onload = function () {
+                resolve(xhr.response);
+            };
+            xhr.send();
+
+        });
+
+    }
+
+    getFileDataByUrl().then(function(response) {
+        var rowsArr = response.split('\n'),
+            outputData = [];
+
+        console.log(rowsArr);
+
+        for (var i = 0; i < rowsArr.length; i++) {
+            outputData.push(rowsArr[i].split(' '))
+
+        }
+       
+        console.log(outputData)
+    })
 }
 
 main();
